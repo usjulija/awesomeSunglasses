d11d4c11a3a64f25a656f3c92cb50658e723edc4
Julia <us.julija@gmail.com>
2019-04-12 14:18:18 +0300

    webinar 4

--- 3-4 Webinar - DOM, Browser Events/solution/js/app.js
+++ 3-4 Webinar - DOM, Browser Events/solution/js/app.js
@@ -157,9 +157,10 @@ const cartCounter = document.getElementById("cartCounter");
 //2.Create a variable that will store the counted number
 let counter = 0;
 //3. Create a funcion that would increase the variable by one
-function counterFunc() {
+function counterFunc(event) {
   counter ++;
   cartCounter.innerHTML = counter;
+  addItemsToCart(event.target.dataset.key);
 }
 //4.grab all the buttons with class "addToCart" and add event listener
 document.querySelectorAll(".addToCart").forEach(button => button.addEventListener("click", counterFunc));
@@ -168,7 +169,7 @@ document.querySelectorAll(".addToCart").forEach(button => button.addEventListene
 //1.Grab the element with id cartButton
 const cartButton = document.getElementById("cartButton");
 //2.Grab the element with class cartSection
-const cartSection = document.querySelector("cartSection");
+const cartSection = document.querySelector(".cartSection");
 //3.Create a function that would: if the cartSection does not contain class visible - add it
 //if the cartSection has class visible - remove it
 function toggeleCartVisibility() {
@@ -179,43 +180,79 @@ function toggeleCartVisibility() {
   }
 }
 //4.add event listener to the cartButton
-
+cartButton.addEventListener("click", toggeleCartVisibility);
 //TODO add items to the cart section
 //1.Grab element with id cartGallery
-
+const cartGallery = document.getElementById("cartGallery");
 //2.Create function addItemsToCart that:
 //- takes an id as the argument
+function addItemsToCart(id) {
   //- filters the cardsArray based on the provided id
-  //- creates new div element with class cart-item. This element has to have 3 children:
-
+  const newItem = cardsArray.filter(card => card.id == id);
+  console.log(newItem);
+  //- creates new div element with class cart-item. This element has to have 4 children:
+  const cartItem = document.createElement("div");
+  cartItem.classList.add("cart-item");
+  cartItem.innerHTML = `
+    <img src=${newItem[0].imgUrl} alt="sunglasses">
+    <p>${newItem[0].name}</p>
+    <p>${priceConverter(newItem[0].price)}</p>
+    <button onclick="removeCartItem(event.target)"><i class="fa fa-close"></i></button>
+  `;
   //--image
   //--paragraph with name
   //--paragraph with price
   //--button with icon X
+  cartGallery.appendChild(cartItem);
+}
 //(hint: the function should be invoked from the counter function, in order to grab the id you kan use e.target.dataset.key)
 
 //TODO remove items from the cart section
 //1. create removeCartItem function, the function should start when the button with icon X in the cart section is clicked
 //the functon should get the clicked element as the argument
+function removeCartItem(element) {
   //-decrease the counter variable by one, update what is shown on the page
+  counter --;
+  cartCounter.innerHTML = counter;
   //-grab the parent of the clicked element, and store it to itemToRemove variable
+  const itemToRemove = element.parentNode.parentNode;
   //-remove the itemToRemove from cartGallery
+  cartGallery.removeChild(itemToRemove);
+}
 
 //TODO make the modal appear
 //1. Grab the element with id modal
+const modal = document.getElementById("modal");
 //2. Create a function that adds visible class to the modal
+function modalVisible(event) {
+  modal.classList.add("visible");
+  updateModal(event.target.dataset.key);
+}
 //3. Grab all the buttons with class openModal and add event listener to them
+document.querySelectorAll(".openModal").forEach(button => button.addEventListener("click", modalVisible));
 
 //TODO close the modal
 //1.Grab the element with id closeModal
+const closeModal = document.getElementById("closeModal");
 //2.Create a function that would remove the class visible from the modal
+function hideModal() {
+  modal.classList.remove("visible");
+}
 //3.Add event listener to the button
+closeModal.addEventListener("click", hideModal);
 
 //TODO update the modal content
 //1. create a function that would take modal id as the argument
-//(hint: the function should be invoked from openModal function, in order to grab the id you kan use e.target.dataset.key)
+function updateModal(id) {
+  //(hint: the function should be invoked from openModal function, in order to grab the id you kan use e.target.dataset.key)
   //-filters the cardsArray based on the id
+  const newItem = cardsArray.filter(card => card.id == id);
   //-changes the source of image with id modalImg
+  document.getElementById("modalImg").src = newItem[0].imgUrl;
   //-changes the content of paragraph with id modalPrice
+  document.getElementById("modalPrice").innerHTML = priceConverter(newItem[0].price);
   //-changes the content of heading with id modalName
+  document.getElementById("modalName").innerHTML = newItem[0].name;
   //changes the content of paragraph with id modalText
+  document.getElementById("modalText").innerHTML = newItem[0].longDescription;
+}
